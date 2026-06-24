315150c525abe7a3f578af44461f33d56e6b68c5
im-amanjai <amanmaadad@gmail.com>
2026-06-24 16:21:20 +0530

    Fix ChromaDB deployment

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -1,19 +1,25 @@
 from langchain_chroma import Chroma
-
 from langchain_community.embeddings import (
     HuggingFaceEmbeddings
 )
+from chromadb.config import Settings
+import tempfile
 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
-
 def create_vector_store(documents):
+
+    persist_dir = tempfile.mkdtemp()
 
     vector_store = Chroma.from_documents(
         documents=documents,
-        embedding=embeddings
+        embedding=embeddings,
+        persist_directory=persist_dir,
+        client_settings=Settings(
+            anonymized_telemetry=False
+        )
     )
 
     return vector_store
